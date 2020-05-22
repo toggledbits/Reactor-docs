a70379833754d37782496792cc11fcba9002cf35
Patrick Rigney <patrick@toggledbits.com>
2020-05-22 11:41:44 -0400

    Fix pathname gen

--- docs/js/oldversion.js
+++ docs/js/oldversion.js
@@ -1,4 +1,4 @@
-var loc = document.location.pathname.replace( "/.+/docs/", "/docs/" );
+var loc = document.location.pathname.replace( /\/docs\/[^\/]+\//, "/docs/" );
 var m = document.createElement('div');
 m.innerHTML = '<p style="font-size: 20px; color: red;"><b>You are reading the documentation for an older version of Reactor. To read documentation for the current version, please go <a href="' + loc + '">here</a>.</b></p>';
 // var content = document.getElementsByClassName("md-content")[0];
