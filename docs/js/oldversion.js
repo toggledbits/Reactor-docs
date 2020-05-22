fff8e52d03b4e62b09d73d9009c868b997b80361
Patrick Rigney <patrick@toggledbits.com>
2020-05-22 11:24:00 -0400

    Old version warning

---
+++ docs/js/oldversion.js
@@ -0,0 +1,7 @@
+var loc = document.location.pathname.replace( "/.+/docs/", "/docs/" );
+var m = document.createElement('div');
+m.innerHTML = '<p style="font-size: 20px; color: red;"><b>You are reading the documentation for an older version of Reactor. To read documentation for the current version, please go <a href="' + loc + '">here</a>.</b></p>';
+// var content = document.getElementsByClassName("md-content")[0];
+// content.parentElement.insertBefore(m, content);
+var content = document.getElementsByTagName("article")[0];
+content.insertBefore(m, content.children[0]);
